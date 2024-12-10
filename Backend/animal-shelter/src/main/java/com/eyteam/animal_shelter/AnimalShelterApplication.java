e2b786fa660c6642bff43e894e44df379ca8221b
zoperalex <alex.michaelides17@gmail.com>
2024-12-10 18:36:40 +0200

    initialized spring boot maven project

---
+++ Backend/animal-shelter/src/main/java/com/eyteam/animal_shelter/AnimalShelterApplication.java
@@ -0,0 +1,13 @@
+package com.eyteam.animal_shelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalShelterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalShelterApplication.class, args);
+	}
+
+}
