0936791f846ec3029eda430544f342af066b2758
zoperalex <alex.michaelides17@gmail.com>
2025-01-18 17:29:38 +0200

    React setup

---
+++ Frontend/webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html',
+        })
+    ],
+    devServer: {
+        port: 5320,
+        open: true,
+        hot: true,
+    }
+};
