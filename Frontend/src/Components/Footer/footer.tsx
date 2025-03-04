0e05b2933a5b33cd1a0aa8ebe2cc3e6a7d366e0c
zoperalex <alex.michaelides17@gmail.com>
2025-03-04 23:10:35 +0200

    Added footer content

--- Frontend/src/Components/Footer/footer.tsx
+++ Frontend/src/Components/Footer/footer.tsx
@@ -4,7 +4,31 @@ import "./footer.css";
 const Footer = () => {
     return (
         <>
-            <div className="footer-container"></div>
+            <div className="footer-container">
+                <div className="footer-content-container">
+                    <div className="logo">
+                        <img className="logo" src="/images/logo.png" alt="" />
+                    </div>
+                    <div className="links-container">
+                        <div className="links">
+                            <ul>
+                                <li>link 1</li>
+                                <li>link 2</li>
+                                <li>link 3</li>
+                                <li>link 4</li>
+                            </ul>
+                        </div>
+                        <div className="links">
+                            <ul>
+                                <li>link 1</li>
+                                <li>link 2</li>
+                                <li>link 3</li>
+                                <li>link 4</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
